92aa8e34de3edc3794b06edc1dd0b5ce8674a253
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2018-07-20 08:55:13 -0700

    Bare minimum server

---
+++ index.js
@@ -0,0 +1,28 @@
+/*
+ * Primary file for API
+ */
+
+// Dependencies
+const http = require('http');
+const url = require('url');
+
+// Server should respond to all requests with a string
+const server = http.createServer((req, res) => {
+  // Get URL and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get path from URL
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Send the response
+  res.end('Hello World\n');
+
+  // Log the path requested
+  console.log('Request received on path: ' + trimmedPath);
+});
+
+// Start the server, and have it listen on port 3000
+server.listen(3000, () => {
+  console.log('The server is listening on port 3000 now.');
+});
