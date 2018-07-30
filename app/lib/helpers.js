86b266c18c71e43f5e6c76b6cfc1f157c1cf6c1d
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2018-07-30 15:14:58 -0700

    Added twilio helper

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -4,6 +4,9 @@
 
 // Dependencies
 const crypto = require('crypto');
+const querystring = require('querystring');
+const https = require('https');
+
 const config = require('./config');
 
 // Container for helpers
@@ -47,6 +50,62 @@ helpers.createRandomString = function(strLength) {
     return str;
   } else {
     return false;
+  }
+}
+
+helpers.sendTwilioSms = function(phone, msg, callback) {
+  // Validate parameters
+  phone = typeof(phone) == 'string' && phone.trim().length == 10 ? phone.trim(): false;
+  msg = typeof(msg) == 'string' && msg.trim().length <= 1600 ? msg.trim() : false;
+  if (phone && msg) {
+    // Configure the request payload
+    const payload = {
+      From: config.twilio.fromPhone,
+      To: '+1' + phone,
+      Body: msg
+    };
+
+    // Stringify the payload
+    const stringPayload = querystring.stringify(payload);
+
+    // Configure the request details
+    const requestDetails = {
+      protocol: 'https:',
+      hostname: 'api.twilio.com',
+      method: 'POST',
+      path: '/2010-04-01/Accounts/' + config.twilio.accountSid + '/Messages.json',
+      auth: config.twilio.accountSid + ':' + config.twilio.authToken,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(stringPayload)
+      }
+    };
+
+    // Instantiate the request object
+    const req = https.request(requestDetails, function(res) {
+      // Grab status of sent request
+      const status = res.statusCode;
+
+      // Callback successfully if request went through
+      if (status == 200 || status == 201) {
+        callback(false);
+      } else {
+        callback('Status code returned was ' + status);
+      }
+    });
+
+    // Bind to the error event so it doesn't get thrown
+    req.on('error', function(e) {
+      callback(e);
+    });
+
+    // Add the payload
+    req.write(stringPayload);
+
+    // End the request
+    req.end();
+  } else {
+    callback('Given parameters missing or invalid');
   }
 }
 
